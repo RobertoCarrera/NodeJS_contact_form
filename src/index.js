33483a8bacdda64a27f8946f8b264edb820d9bb1
RobertoCarrera <robertocarrera93@gmail.com>
2024-01-18 03:29:30 +0100

    cors changed

--- src/index.js
+++ src/index.js
@@ -4,27 +4,22 @@ const path = require('path');
 const app = express();
 require('dotenv').config();
 
-const allowedIp = process.env.ALLOWED_IP;
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+    res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    next();
+});
 
-const corsOptions = {
-    origin: function (origin, callback) {
-        
-        callback(null, true);
-    }
-};
-
-app.use(cors(corsOptions));
 app.use(express.urlencoded({extended: false}));
 app.use(express.json());
 app.use(require('./routes/index'));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.get('', (req, res) => {
-
     res.json({ message: 'Hola, mundo!' });
 });
 
 app.listen(3500, () => {
-
     console.log("Server working on port 3500");
 });
